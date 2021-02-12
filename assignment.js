77dffd972e391ad586c0bd306d97e6a4b6b5fbd5
khalidasad-web <khalid.ibnal.asad@gmail.com>
2021-02-12 06:04:14 +0000

    2nd-Commit-12-Feb-2021

--- assignment.js
+++ assignment.js
@@ -2,14 +2,17 @@ function kilometerToMeter() {
     // Kilometer to Meter conversion function
     // return meter
 }
+
 function budgetCalculator() {
     // Parameter watch quantity, phone quantity , laptop quantity
     // return totalPrice
 }
+
 function hotelCost() {
     // Parameter number of days
     // return totalCost
 }
+
 function megaFriend() {
     // Parameter array of friends
     // return largest name
